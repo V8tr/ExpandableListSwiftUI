002c19acebcb33ed3ac6b15c65f3a7bde53fa5fe
V8tr <ne0_n@mail.ru>
2020-01-29 22:56:54 +0200

    Initial commit

---
+++ ExpandableListSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExpandableListSwiftUI
+//
+//  Created by Vadim Bulavin on 1/29/20.
+//  Copyright © 2020 Vadim Bulavin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
